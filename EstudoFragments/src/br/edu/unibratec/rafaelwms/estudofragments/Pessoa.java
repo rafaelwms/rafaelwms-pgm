61a427252edf2fc6f93d168ef65811ab2f1e572c
rafael.wmsa <rafael.wmsa@3f06a582-059f-0c88-b94b-38cf6c4a28cf>
2014-04-02 23:15:11 +0000

    Aula de hoje

---
+++ EstudoFragments/src/br/edu/unibratec/rafaelwms/estudofragments/Pessoa.java
@@ -0,0 +1,24 @@
+package br.edu.unibratec.rafaelwms.estudofragments;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable{
+	
+	public String nome;
+	public String sobrenome;
+	
+	
+	public Pessoa(String nome, String sobrenome) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}
+
+
+	@Override
+	public String toString() {
+		return nome;
+	}
+	
+	
+
+}
