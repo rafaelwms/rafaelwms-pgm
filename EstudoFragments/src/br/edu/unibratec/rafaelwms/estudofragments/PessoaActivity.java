11133e4090122af0132574d71e96ca8150256fa3
rafael.wmsa <rafael.wmsa@3f06a582-059f-0c88-b94b-38cf6c4a28cf>
2014-04-03 00:01:17 +0000

    kjdhvisdhvi

--- EstudoFragments/src/br/edu/unibratec/rafaelwms/estudofragments/PessoaActivity.java
+++ EstudoFragments/src/br/edu/unibratec/rafaelwms/estudofragments/PessoaActivity.java
@@ -1,11 +1,13 @@
 package br.edu.unibratec.rafaelwms.estudofragments;
 
+import br.edu.unibratec.rafaelwms.estudofragments.PessoaListFragment.AoClicarNaPessoaListener;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class PessoaActivity extends FragmentActivity {
+public class PessoaActivity extends FragmentActivity implements AoClicarNaPessoaListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,18 @@ public class PessoaActivity extends FragmentActivity {
         setContentView(R.layout.activity_pessoa);
     }
 
+	@Override
+	public void clicouNaPessoa(Pessoa pessoa) {
+		if(isTablet()){
+			
+		}else{
+			Intent it = new Intent(this, DetalhePessoaActivity.class);
+			it.putExtra("pessoa", pessoa);
+			startActivity(it);
+			
+		}
+	}
 
-   
+
+   private boolean isTablet(){return false;}
 }
