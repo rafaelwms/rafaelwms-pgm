87243cef5c6859ccc50f6e40b0af9fa981e9c27e
rafael.wmsa@gmail.com <rafael.wmsa@gmail.com@3f06a582-059f-0c88-b94b-38cf6c4a28cf>
2014-05-28 11:49:44 +0000

    DB pronto (vale testar)

--- AskAndAnswer/src/br/edu/unibratec/rafaelwms/askandanswer/ManagementActivity.java
+++ AskAndAnswer/src/br/edu/unibratec/rafaelwms/askandanswer/ManagementActivity.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -17,9 +18,11 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.os.Build;
 
-public class ManagementActivity extends ActionBarActivity implements OnItemClickListener{
+public class ManagementActivity extends ActionBarActivity implements
+		OnItemClickListener {
 
 	User loggedUser;
 	EditText edtTestName;
@@ -27,35 +30,38 @@ public class ManagementActivity extends ActionBarActivity implements OnItemClick
 	EditText edtTestNote;
 	Test testSelected;
 	List<Test> tests;
-	TestDB db;
+	AskAndAnswerDB db;
 	ListView testList;
 	TestAdapter adapter;
-
+	public static final String SELECTED_TEST = "testSelected"; 
+	public static final int REQUEST_NEW_QUESTION = 132;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_management);
-		
-		db = new TestDB(this);
-		
+
+		db = new AskAndAnswerDB(this);
+
 		Intent it = getIntent();
-		
+
 		loggedUser = (User) it.getSerializableExtra(MainActivity.SESSION);
-		
-		
+
 		testSelected = null;
 		edtTestName = (EditText) findViewById(R.id.editTestName);
 		edtTestCategory = (EditText) findViewById(R.id.editNewTestCategory);
 		edtTestNote = (EditText) findViewById(R.id.editNewTestNote);
 		testList = (ListView) findViewById(R.id.litsViewTests);
 		testList.setOnItemClickListener(this);
-		
+
 		tests = new ArrayList<>();
+
+		tests = db.findTestByUser(loggedUser);
 		
-		tests = db.findTestByUser(loggedUser);;
-		
-		if(tests.size() > 0){
-		refreshTestList();		
+
+		if (tests.size() > 0) {
+			refreshTestList();
 		}
 	}
 
@@ -68,48 +74,100 @@ public class ManagementActivity extends ActionBarActivity implements OnItemClick
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if (id == R.id.save_test_action) {
 
-			Test test = new Test(MainActivity.loggedUser, edtTestName.getText()
-					.toString(), edtTestCategory.getText().toString(), null,
-					Double.parseDouble(edtTestNote.getText().toString()));
-			
-			db.insert(test);
-			refreshTestList();
-			clearEditTexts();
-			
+		if (item.getItemId() == R.id.save_test_action) {
+			try {
 
+				if (testSelected == null || testSelected.getId_test() <= 0) {
+
+					Test test = new Test(MainActivity.loggedUser, edtTestName
+							.getText().toString(), edtTestCategory.getText()
+							.toString(), null, Double.parseDouble(edtTestNote
+							.getText().toString()));
+
+					db.insertTest(test);
+					refreshTestList();
+					clearEditTexts();
+					testSelected = new Test();
+					return true;
+				} else {
+
+					Test test = new Test(
+							testSelected.getId_test(),
+							MainActivity.loggedUser,
+							edtTestName.getText().toString(),
+							edtTestCategory.getText().toString(),
+							testSelected.getQuestions(),
+							Double.parseDouble(edtTestNote.getText().toString()));
+
+					db.alterTest(test);
+					refreshTestList();
+					clearEditTexts();
+					testSelected = new Test();
+					return true;
+				}
+
+			} catch (Exception ex) {
+				return true;
+			}
+		}
+		if (item.getItemId() == R.id.add_question_action) {
+			if (testSelected == null || testSelected.getId_test() <= 0){
+				Toast.makeText(this, getResources().getString(R.string.exceptionMustSelectTest), Toast.LENGTH_SHORT)
+				.show();
+			}else{
+			Intent it = new Intent(this, ManageQuestionActivity.class);
+			it.putExtra(SELECTED_TEST, testSelected);
+			startActivityForResult(it, REQUEST_NEW_QUESTION);
+			return true;
+			}
+		}
+		
+		if (item.getItemId() == R.id.del_test_action) {
+			
+			if (testSelected == null || testSelected.getId_test() <= 0){
+				Toast.makeText(this, getResources().getString(R.string.exceptionMustSelectTest), Toast.LENGTH_SHORT)
+				.show();
+			}else{	
+				db.deleteTest(testSelected);
+				testSelected = new Test();
+				refreshTestList();
+				clearEditTexts();
+			}
 			return true;
 		}
-		if (item.getItemId() == R.id.logout_action) {
-			MainActivity.loggedUser = null;
-			Intent it = new Intent(this, LoginActivity.class);
-			startActivity(it);
-			finish();
-			return true;
-		}
+		
 		return super.onOptionsItemSelected(item);
 	}
-	
-	private void refreshTestList(){
+
+	private void refreshTestList() {
 		tests = new ArrayList<Test>();
 		tests = db.findTestByUser(loggedUser);
 		adapter = new TestAdapter(tests);
 		testList.setAdapter(adapter);
 	}
-	
-	private void clearEditTexts(){
+
+	private void clearEditTexts() {
 		edtTestName.setText("");
 		edtTestCategory.setText("");
 		edtTestNote.setText("");
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		
-		
-		
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+
+		testSelected = (Test) testList.getItemAtPosition(position);
+		edtTestName.setText(testSelected.getText_title());
+		edtTestCategory.setText(testSelected.getCategory());
+		edtTestNote.setText(testSelected.getTest_value()+"");
+
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
 }
